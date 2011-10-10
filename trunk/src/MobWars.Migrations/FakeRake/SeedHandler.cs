b6dd4164a32e194c89b29aaa8f9f5964ea09be4d
michel.grootjans <michel.grootjans@425f0b56-8098-9889-3b83-a00c807c5f7a>
2011-10-10 15:09:50 +0000

    demoed hibernate, migrator, automapper

--- trunk/src/MobWars.Migrations/FakeRake/SeedHandler.cs
+++ trunk/src/MobWars.Migrations/FakeRake/SeedHandler.cs
@@ -22,8 +22,8 @@ namespace MobWars.Migrations.FakeRake
             var scott = new Player("scott", "tiger", "Scott Adams");
             scott.Buy(new Knife("Rusty knife", 2));
             scott.Buy(new Sword("Broken shortsword", 8));
-
             session.Save(scott);
+            
             session.Save(new Sword("Rusty shortsword", 14));
             session.Save(new Sword("Shiny shortsword", 42));
         }
--- trunk/src/MobWars.Test.Unit/AutoMapper/AutomapperDemo.cs
+++ trunk/src/MobWars.Test.Unit/AutoMapper/AutomapperDemo.cs
@@ -13,7 +13,25 @@ namespace MobWars.Test.Unit.AutoMapper
         [Test]
         public void should_map_customer()
         {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Order, OrderDto>();
+
             var customer = new Customer("agileminds");
+            customer.Order(2, new Product("planning poker cards", 0.95));
+
+            var customerDto = Mapper.Map<Customer, CustomerDto>(customer);
+            Assert.That(customerDto.Name, Is.EqualTo("agileminds"));
+            Assert.That(customerDto.OrdersCount, Is.EqualTo(1));
         }
+    }
+
+    public class CustomerDto
+    {
+        public string Name { get; set; }
+        public int OrdersCount { get; set; }
+    }
+
+    public class OrderDto
+    {
     }
 }
